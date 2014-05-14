28217f681979ffd2eb4ba5fffb319b1e62377be3
jjvanzon <janjoostvanzon@hotmail.com>
2014-05-14 21:59:46 +0000

    SetText working on phone deployments

--- Apps/JJ.Apps.SetText.Unity.Offline/Copy .NET Debug binaries.bat
+++ Apps/JJ.Apps.SetText.Unity.Offline/Copy .NET Debug binaries.bat
@@ -5,14 +5,16 @@ rmdir /S /Q Assets\Libraries
 mkdir Assets\Libraries
 xcopy ..\JJ.Apps.SetText.Unity.DotNet\bin\Debug\*.* Assets\Libraries /E /Q
 
-REM Delete dll's that indirectly reference System.Net.Sockets, 
-REM which is not supported by Unity free if you want to deploy.
+REM  Delete dll's that indirectly reference System.Net.Sockets, 
+REM  which is not supported by Unity free if you want to deploy.
 
 del Assets\Libraries\JJ.Apps.SetText.AppService.Interface.dll
 del Assets\Libraries\JJ.Apps.SetText.Unity.DotNet.dll
 
 del Assets\Libraries\JJ.Apps.SetText.AppService.Interface.pdb
 del Assets\Libraries\JJ.Apps.SetText.Unity.DotNet.pdb
+
+REM  TODO: COPY SATELLITE ASSEMBLIES TO WINDOWS PHONE DEPLOYMENT FOLDER!
 
 pause
 
--- Apps/JJ.Apps.SetText/PresenterInterfaces/ISetTextPresenter.cs
+++ Apps/JJ.Apps.SetText/PresenterInterfaces/ISetTextPresenter.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
 using System.Text;
 
 namespace JJ.Apps.SetText.PresenterInterfaces
