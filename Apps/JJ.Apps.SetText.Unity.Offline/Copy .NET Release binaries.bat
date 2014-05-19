727edf8227ffa0bf5c6d48ae1de3afe79de7a42a
jjvanzon <janjoostvanzon@hotmail.com>
2014-05-19 20:48:52 +0000

    SetText Offline

--- Apps/JJ.Apps.SetText.Unity.Offline/Copy .NET Release binaries.bat
+++ Apps/JJ.Apps.SetText.Unity.Offline/Copy .NET Release binaries.bat
@@ -1,19 +1,20 @@
 @echo off
 prompt $G
 @echo on
-rmdir /S /Q Assets\Libraries 
-mkdir Assets\Libraries
-xcopy ..\JJ.Apps.SetText.Unity.DotNet\bin\Release\*.* Assets\Libraries /E /Q
+rmdir /S /Q Assets\DotNetAssemblies 
+mkdir Assets\DotNetAssemblies
+xcopy ..\JJ.Apps.SetText.Unity.DotNet\bin\Release\*.* Assets\DotNetAssemblies /E /Q
 
 REM  Delete dll's that indirectly reference System.Net.Sockets, 
-REM  which is not supported by Unity free if you want to deploy.
+REM  which is not supported by Unity Free if you want to deploy.
 
-del Assets\Libraries\JJ.Apps.SetText.AppService.Interface.dll
-del Assets\Libraries\JJ.Apps.SetText.Unity.DotNet.dll
+del Assets\DotNetAssemblies\JJ.Apps.SetText.AppService.Interface.dll
+del Assets\DotNetAssemblies\JJ.Apps.SetText.Unity.DotNet.dll
 
-del Assets\Libraries\JJ.Apps.SetText.AppService.Interface.pdb
-del Assets\Libraries\JJ.Apps.SetText.Unity.DotNet.pdb
+del Assets\DotNetAssemblies\JJ.Apps.SetText.AppService.Interface.pdb
+del Assets\DotNetAssemblies\JJ.Apps.SetText.Unity.DotNet.pdb
 
-REM  TODO: COPY SATELLITE ASSEMBLIES TO WINDOWS PHONE DEPLOYMENT FOLDER!
+REM  TODO FOR WINDOWS PHONE: CLEAR THE ASSETS\MONO FOLDER!!!
+REM  TODO FOR WINDOWS PHONE: COPY SATELLITE ASSEMBLIES TO WINDOWS PHONE DEPLOYMENT FOLDER!!!
 
 pause
