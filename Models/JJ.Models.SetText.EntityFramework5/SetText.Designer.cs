7b67c89c2c2c8da769af088c600e6be146fcb77c
jjvanzon <janjoostvanzon@hotmail.com>
2014-12-08 21:33:18 +0000

    Refactoring

---
+++ Models/JJ.Models.SetText.EntityFramework5/SetText.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\JJ\Dev\1. Products\2. Code\1. Software System\X\9. JJ\Models\JJ.Models.SetText.EntityFramework5\SetText.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ Models/JJ.Models.SetText.Xml/Mapping/EntityMapping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JJ.Framework.Persistence.Xml;
+
+namespace JJ.Models.SetText.Xml.Mapping
+{
+    public class EntityMapping : XmlMapping<Entity>
+    {
+        public EntityMapping()
+        {
+            IdentityPropertyName = "ID";
+            IdentityType = IdentityType.AutoIncrement;
+        }
+    }
+}
