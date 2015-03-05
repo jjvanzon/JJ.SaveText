d2a48d4b276674d05bf90efdce1eda060c71c9a1
jjvanzon <janjoostvanzon@hotmail.com>
2015-03-05 21:35:02 +0000

    Synthesizer: finish Curve entity model

--- Persistence/JJ.Persistence.SetText.Memory/Mappings/EntityMapping.cs
+++ Persistence/JJ.Persistence.SetText.Memory/Mappings/EntityMapping.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using JJ.Framework.Persistence.Memory;
 
-namespace JJ.Persistence.SetText.Memory.Mapping
+namespace JJ.Persistence.SetText.Memory.Mappings
 {
     public class EntityMapping : MemoryMapping<Entity>
     {
