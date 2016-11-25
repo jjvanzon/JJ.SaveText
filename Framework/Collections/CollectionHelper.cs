60a26ea01ff4bb0ce4e906f4c8607245a13cfaa5
Jan-Joost van Zon <janjoostvanzon79@gmail.com>
2016-11-25 23:19:32 +0100

    Add files via upload

---
+++ Framework/Collections/CollectionHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace JJ.Framework.Collections
+{
+    public static class CollectionHelper
+    {
+        /// <summary>
+        /// This BinarySearch has not been thoroughly (unit) tested.
+        /// It has been used in the development of audio processing, 
+        /// during which it was debugged, and produced expected audio output.
+        /// The biggest concern is what happens with integer roundoff for certain edge cases.
+        /// </summary>            
+        /// <param name="sortedArray"> Not checked for null, for performance. </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void BinarySearchInexact(
+            double[] sortedArray,
+            double input,
+            out double valueBefore,
+            out double valueAfter)
+        {
+            int count = sortedArray.Length;
+            double min = sortedArray[0];
+            double max = sortedArray[count - 1];
+            int halfCount = count >> 1;
+
+            BinarySearchInexact(sortedArray, halfCount, min, max, input, out valueBefore, out valueAfter);
+        }
+
+        /// <summary>
+        /// Overload with more values you supply yourself: halfLength, min and max,
+        /// that you could cache yourself for performance.
+        /// 
+        /// This BinarySearch has not been thoroughly (unit) tested.
+        /// It has been used in the development of audio processing, 
+        /// during which it was debugged, and produced expected audio output.
+        /// The biggest concern is what happens with integer roundoff for certain edge cases.
+        /// </summary>
+        /// <param name="sortedArray"> Not checked for null, for performance. </param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void BinarySearchInexact(
+            double[] sortedArray,
+            int halfCount,
+            double min,
+            double max,
+            double input,
+            out double valueBefore,
+            out double valueAfter)
+        {
+            int sampleIndex = halfCount;
+            int jump = halfCount;
+
+            valueBefore = min;
+            valueAfter = max;
+
+            while (jump != 0)
+            {
+                double sample = sortedArray[sampleIndex];
+
+                jump = jump >> 1;
+
+                if (input >= sample)
+                {
+                    valueBefore = sample;
+
+                    sampleIndex += jump;
+                }
+                else
+                {
+                    valueAfter = sample;
+
+                    sampleIndex -= jump;
+                }
+            }
+        }
+    }
+}
---
+++ Framework/Collections/RedBlackTreeNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JJ.Framework.Reflection.Exceptions;
+
+namespace JJ.Framework.Collections
+{
+    internal class RedBlackTreeNode<TKey, TValue>
+        where TKey : IComparable
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public RedBlackTreeColorEnum Color { get; set; }
+        public RedBlackTreeNode<TKey, TValue> Left { get; set; }
+        public RedBlackTreeNode<TKey, TValue> Right { get; set; }
+        public RedBlackTreeNode<TKey, TValue> Parent { get; set; }
+
+        public RedBlackTreeNode(TKey key, TValue value, RedBlackTreeColorEnum color)
+        {
+            Key = key;
+            Value = value;
+            Color = color;
+        }
+        public RedBlackTreeNode<TKey, TValue> GetGrandparent()
+        {
+            if (Parent == null) throw new NullException(() => Parent); // Not the root node
+            if (Parent.Parent == null) throw new NullException(() => Parent.Parent);  // Not child of root
+
+            return Parent.Parent;
+        }
+
+        public RedBlackTreeNode<TKey, TValue> TryGetSibling()
+        {
+            if (Parent == null) throw new NullException(() => Parent);
+
+            if (this == Parent.Left)
+            {
+                return Parent.Right;
+            }
+            else
+            {
+                return Parent.Left;
+            }
+        }
+
+        public RedBlackTreeNode<TKey, TValue> GetUncle()
+        {
+            if (Parent == null) throw new NullException(() => Parent); // Root node has no uncle
+            if (Parent.Parent == null) throw new NullException(() => Parent.Parent); // Children of root have no uncle
+
+            return Parent.TryGetSibling();
+        }
+    }
+}
