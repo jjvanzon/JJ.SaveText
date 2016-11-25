8b1034978ff26357d9f4d070b2dd18944a6e1fcd
Jan-Joost van Zon <janjoostvanzon79@gmail.com>
2016-11-25 23:17:13 +0100

    Add files via upload

---
+++ Framework/Business.Tests/Helpers/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JJ.Framework.Business.Tests.Helpers
+{
+    internal class Entity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Entity RelatedEntity { get; set; }
+
+        public Entity QuestionType { get; set; }
+        public Entity Source { get; set; }
+        public IList<Entity> QuestionCategories { get; set; }
+        public IList<Entity> QuestionLinks { get; set; }
+        public IList<Entity> QuestionFlags { get; set; }
+    }
+}
