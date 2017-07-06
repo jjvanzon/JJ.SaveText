ea1fd273d13ed259e7ea01dd409807eb07b0d882
jjvanzon <jjvanzon79@gmail.com>
2017-07-06 19:54:34 +0200

    arch docs

---
+++ Data/Canonical/Helpers/DebuggerDisplayFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JJ.Data.Canonical.Helpers
+{
+    internal static class DebuggerDisplayFormatter
+    {
+        public static string GetDebuggerDisplay(IDAndName idAndName)
+        {
+            if (idAndName == null) throw new ArgumentNullException(nameof(idAndName));
+
+            string debuggerDisplay = $"{{{nameof(IDAndName)}}} {idAndName.ID} {idAndName.Name}";
+            return debuggerDisplay;
+        }
+
+        internal static string GetDebuggerDisplay(MessageDto message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+
+            string debuggerDisplay = $"{{{nameof(MessageDto)}}} {message.Key} - '{message.Text}'";
+            return debuggerDisplay;
+        }
+    }
+}
---
+++ Framework/Business/Helpers/DebuggerDisplayFormatter.cs
@@ -0,0 +1,77 @@
+﻿using JJ.Framework.Common;
+using System;
+using System.Text;
+
+namespace JJ.Framework.Business.Helpers
+{
+    internal static class DebuggerDisplayFormatter
+    {
+        public static string GetDebuggerDisplay(Message message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+
+            string debuggerDisplay = $"{{{nameof(Message)}}} {FormatMessage(message)}";
+            return debuggerDisplay;
+        }
+
+        public static string GetDebuggerDisplay(Messages messages)
+        {
+            if (messages == null) throw new ArgumentNullException(nameof(messages));
+
+            string debuggerDisplay = $"{{{nameof(Messages)}}} {FormatMessages(messages)}";
+            return debuggerDisplay;
+        }
+
+        public static string GetDebuggerDisplay(IResult result)
+        {
+            if (result == null) throw new ArgumentNullException(nameof(result));
+
+            var sb = new StringBuilder();
+
+            sb.Append($"{{{result.GetType().Name}}}");
+
+            if (result.Successful)
+            {
+                sb.Append($" {nameof(result.Successful)}");
+            }
+            else
+            {
+                sb.Append($" Not {nameof(result.Successful)}");
+            }
+
+            if (result.Messages != null)
+            {
+                string formattedMessages = FormatMessages(result.Messages);
+                if (!string.IsNullOrWhiteSpace(formattedMessages))
+                {
+                    sb.Append($": {formattedMessages}");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private static string FormatMessage(Message message) => $"{message.Key} - '{message.Text}'";
+
+        private static string FormatMessages(Messages messages)
+        {
+            var sb = new StringBuilder();
+
+            foreach (Message message in messages)
+            {
+                if (message != null)
+                {
+                    sb.Append($"{FormatMessage(message)}, ");
+                }
+                else
+                {
+                    sb.Append("<null>, ");
+                }
+            }
+
+            string formattedMessages = sb.ToString().TrimEnd(", ");
+
+            return formattedMessages;
+        }
+    }
+}
